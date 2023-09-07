9b9ff2bead7f50fc098ba50bc6a33e38611bb20c
Ajaydev8 <131373049+Ajaydev8@users.noreply.github.com>
2023-09-07 15:39:24 +0530

    Added shape,color,movement

--- Day-18-start/main.py
+++ Day-18-start/main.py
@@ -1,2 +1,22 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
+# Getting hold of the turtle class
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("navy")
+timmy.forward(10)
+
+
+
+
+
+
+
+
+
+
+
+# Getting hold of the screen
+screen = Screen()
+# This will prevent the screen from closing, It is only closed when clicked
+screen.exitonclick()
