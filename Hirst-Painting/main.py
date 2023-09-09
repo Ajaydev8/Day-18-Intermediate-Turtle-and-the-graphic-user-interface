0d417848cd0648d5a704e28ff66eb311d3e560a8
Ajaydev8 <131373049+Ajaydev8@users.noreply.github.com>
2023-09-09 13:58:55 +0530

    drew the first line

--- Hirst-Painting/main.py
+++ Hirst-Painting/main.py
@@ -1,8 +1,12 @@
 import colorgram
+from turtle import Turtle, Screen
+import random
 
 # Extraction  the colors from an image
 colors = colorgram.extract('download.jpg', 30)
 colors_list = []
+screen = Screen()
+tim = Turtle()
 
 
 for color in colors:
@@ -17,3 +21,19 @@ rgb_colors = [(236, 244, 244), (236, 224, 224), (197, 7, 7), (195, 164, 164), (2
               (212, 135, 135), (233, 223, 223), (199, 33, 33), (13, 183, 183), (230, 166, 166), (126, 189, 189),
               (8, 49, 49), (40, 132, 132), (128, 219, 219), (58, 12, 12), (67, 22, 22), (114, 90, 90), (146, 216, 216),
               (179, 17, 17), (233, 66, 66)]
+
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
+
+tim.penup()
+tim.goto(-250, -250)
+tim.speed("fastest")
+
+for _ in range(10):
+    random_rgb_color = random.choice(rgb_colors)
+    hex_color = rgb_to_hex(random_rgb_color)  # Convert RGB to Hex
+    tim.color(hex_color)
+    tim.dot(20)
+    tim.forward(50)
+
+screen.exitonclick()
